293fe935aa74bfab8d4d5ccf1c1976ceff6bd777
anil2105 <anil.krtg@gmail.com>
2022-11-03 22:43:56 +0530

    This is my first commit

---
+++ src/test/java/com/anil_practice2/scripts/Demo.java
@@ -0,0 +1,28 @@
+package com.anil_practice2.scripts;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Demo 
+{
+	public static void main(String[] args) throws InterruptedException
+	{
+	//	System.setProperty("webdriver.chrome.driver", ".//drivers/chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+		driver.get("");
+		
+		
+		
+		Thread.sleep(3000);
+		driver.close();
+		
+	}		
+}
